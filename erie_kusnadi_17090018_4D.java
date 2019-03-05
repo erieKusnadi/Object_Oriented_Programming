022cbd6e4538749e30c01c5d110b02a13d666489
erieKusnadi <48240671+erieKusnadi@users.noreply.github.com>
2019-03-05 20:00:04 +0700

    Update erie_kusnadi_17090018_4D.java

--- erie_kusnadi_17090018_4D.java
+++ erie_kusnadi_17090018_4D.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package output;
 
-/**
- *
- * @author Olipe
- */
 public class erie_kusnadi_17090018_4D {
   public static void main(String[] args) {
         System.out.println("selamat datang di object oriented programing \n erie_kusnadi_17090018_4D");  
